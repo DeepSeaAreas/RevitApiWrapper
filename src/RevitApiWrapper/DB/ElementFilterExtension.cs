14d14b66f6513388f2a2c6c45076ab992a73449c
Young <iamlovedit@163.com>
2022-05-19 20:55:38 +0800

    refactor projects (#3)
    * add filter class
    
    * add new configurations
    
    * completed the element collector
    
    * update git ignore
    
    * refactor projects
    
    * modify project configuration
    
    * update codes

---
+++ src/RevitApiWrapper/DB/ElementFilterExtension.cs
@@ -0,0 +1,124 @@
+﻿using Autodesk.Revit.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitApiWrapper.DB
+{
+    public static class ElementFilterExtension
+    {
+        /// <summary>
+        /// Create a ElementCollector <see cref="Autodesk.Revit.DB.FilteredElementCollector"/>
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="view"></param>
+        /// <returns></returns>
+        private static FilteredElementCollector GetCollector(Document doc, View view = null)
+        {
+            return view == null ? new FilteredElementCollector(doc) : new FilteredElementCollector(doc, view.Id);
+        }
+
+        /// <summary>
+        /// Collect elements by type of element, If view is not null,Only the visible elements of the current view 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="doc"></param>
+        /// <param name="view"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IEnumerable<T> GetElementsByClass<T>(this Document doc, View view = null) where T : Element
+        {
+            if (doc == null)
+            {
+                throw new ArgumentNullException(nameof(doc));
+            }
+            using (var collector = GetCollector(doc, view))
+            {
+                return collector.OfClass(typeof(T)).OfType<T>();
+            }
+        }
+
+        /// <summary>
+        /// Collect elements by category of element, If view is not null,Only the visible elements of the current view 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="doc"></param>
+        /// <param name="builtInCategory"></param>
+        /// <param name="view"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public static IEnumerable<T> GetElementsByCategory<T>(this Document doc, BuiltInCategory builtInCategory, View view = null) where T : Element
+        {
+            if (doc == null)
+            {
+                throw new ArgumentNullException(nameof(doc));
+            }
+            if (builtInCategory == BuiltInCategory.INVALID)
+            {
+                throw new ArgumentException(nameof(builtInCategory));
+            }
+
+            using (var collector = GetCollector(doc, view))
+            {
+                return collector.OfCategory(builtInCategory).OfType<T>();
+            }
+        }
+
+        /// <summary>
+        /// Collect elements by category of element, If view is not null,Only the visible elements of the current view 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="doc"></param>
+        /// <param name="category"></param>
+        /// <param name="view"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IEnumerable<T> GetElementsByCategory<T>(this Document doc, Category category, View view = null) where T : Element
+        {
+            if (doc == null)
+            {
+                throw new ArgumentNullException(nameof(doc));
+            }
+            if (category == null)
+            {
+                throw new ArgumentNullException(nameof(category));
+            }
+
+            using (var collector = GetCollector(doc, view))
+            {
+                return collector.OfCategoryId(category.Id).OfType<T>();
+            }
+        }
+
+        /// <summary>
+        /// Collect elements by a element filter, If view is not null,Only the visible elements of the current view 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="doc"></param>
+        /// <param name="filter"></param>
+        /// <param name="view"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IEnumerable<T> GetElementsByFilter<T>(this Document doc, ElementFilter filter, View view = null) where T : Element
+        {
+            if (doc is null)
+            {
+                throw new ArgumentNullException(nameof(doc));
+            }
+
+            if (filter is null)
+            {
+                throw new ArgumentNullException(nameof(filter));
+            }
+            using (var collector = GetCollector(doc, view))
+            using (filter)
+            {
+                return collector.WherePasses(filter).OfType<T>();
+            }
+
+        }
+    }
+}
